649da88ea91f2e4c60f8c9081154924206717b91
KienLoveNH <kientri098@gmail.com>
2025-03-10 07:51:24 +0700

    "create login, forgot password and completing state computer"

---
+++ NetStore/Form/FormStateComputer/AddComputer.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace NetStore.Form.FormStateComputer
+{
+    partial class AddComputer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNamecomputer = new System.Windows.Forms.Label();
+            this.lblTypecomputer = new System.Windows.Forms.Label();
+            this.btnAddcomputer = new Guna.UI2.WinForms.Guna2Button();
+            this.btnBack = new Guna.UI2.WinForms.Guna2Button();
+            this.txtNamecomputer = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtTypecomputer = new Guna.UI2.WinForms.Guna2TextBox();
+            this.SuspendLayout();
+            // 
+            // lblNamecomputer
+            // 
+            this.lblNamecomputer.AutoSize = true;
+            this.lblNamecomputer.Font = new System.Drawing.Font("Cooper Black", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNamecomputer.Location = new System.Drawing.Point(72, 82);
+            this.lblNamecomputer.Name = "lblNamecomputer";
+            this.lblNamecomputer.Size = new System.Drawing.Size(135, 32);
+            this.lblNamecomputer.TabIndex = 0;
+            this.lblNamecomputer.Text = "Tên máy";
+            // 
+            // lblTypecomputer
+            // 
+            this.lblTypecomputer.AutoSize = true;
+            this.lblTypecomputer.Font = new System.Drawing.Font("Cooper Black", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTypecomputer.Location = new System.Drawing.Point(72, 174);
+            this.lblTypecomputer.Name = "lblTypecomputer";
+            this.lblTypecomputer.Size = new System.Drawing.Size(142, 32);
+            this.lblTypecomputer.TabIndex = 1;
+            this.lblTypecomputer.Text = "Loại máy";
+            // 
+            // btnAddcomputer
+            // 
+            this.btnAddcomputer.BorderRadius = 3;
+            this.btnAddcomputer.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddcomputer.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnAddcomputer.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnAddcomputer.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnAddcomputer.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(175)))), ((int)(((byte)(250)))));
+            this.btnAddcomputer.Font = new System.Drawing.Font("Cooper Black", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddcomputer.ForeColor = System.Drawing.Color.White;
+            this.btnAddcomputer.Location = new System.Drawing.Point(309, 269);
+            this.btnAddcomputer.Name = "btnAddcomputer";
+            this.btnAddcomputer.Size = new System.Drawing.Size(180, 55);
+            this.btnAddcomputer.TabIndex = 2;
+            this.btnAddcomputer.Text = "Thêm máy";
+            // 
+            // btnBack
+            // 
+            this.btnBack.BorderRadius = 3;
+            this.btnBack.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnBack.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnBack.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnBack.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnBack.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(108)))), ((int)(((byte)(99)))));
+            this.btnBack.Font = new System.Drawing.Font("Cooper Black", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.White;
+            this.btnBack.Location = new System.Drawing.Point(579, 269);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(180, 55);
+            this.btnBack.TabIndex = 3;
+            this.btnBack.Text = "Trở lại";
+            // 
+            // txtNamecomputer
+            // 
+            this.txtNamecomputer.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtNamecomputer.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtNamecomputer.DefaultText = "";
+            this.txtNamecomputer.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtNamecomputer.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtNamecomputer.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNamecomputer.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtNamecomputer.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.txtNamecomputer.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtNamecomputer.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtNamecomputer.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtNamecomputer.Location = new System.Drawing.Point(259, 64);
+            this.txtNamecomputer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtNamecomputer.Name = "txtNamecomputer";
+            this.txtNamecomputer.PlaceholderText = "";
+            this.txtNamecomputer.SelectedText = "";
+            this.txtNamecomputer.Size = new System.Drawing.Size(466, 50);
+            this.txtNamecomputer.TabIndex = 4;
+            // 
+            // txtTypecomputer
+            // 
+            this.txtTypecomputer.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtTypecomputer.DefaultText = "";
+            this.txtTypecomputer.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtTypecomputer.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtTypecomputer.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtTypecomputer.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtTypecomputer.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(217)))), ((int)(((byte)(217)))));
+            this.txtTypecomputer.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtTypecomputer.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.txtTypecomputer.ForeColor = System.Drawing.Color.White;
+            this.txtTypecomputer.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtTypecomputer.Location = new System.Drawing.Point(259, 156);
+            this.txtTypecomputer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTypecomputer.Name = "txtTypecomputer";
+            this.txtTypecomputer.PlaceholderText = "";
+            this.txtTypecomputer.SelectedText = "";
+            this.txtTypecomputer.Size = new System.Drawing.Size(466, 50);
+            this.txtTypecomputer.TabIndex = 5;
+            // 
+            // AddComputer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 343);
+            this.Controls.Add(this.txtTypecomputer);
+            this.Controls.Add(this.txtNamecomputer);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnAddcomputer);
+            this.Controls.Add(this.lblTypecomputer);
+            this.Controls.Add(this.lblNamecomputer);
+            this.Name = "AddComputer";
+            this.Text = "AddComputer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNamecomputer;
+        private System.Windows.Forms.Label lblTypecomputer;
+        private Guna.UI2.WinForms.Guna2Button btnAddcomputer;
+        private Guna.UI2.WinForms.Guna2Button btnBack;
+        private Guna.UI2.WinForms.Guna2TextBox txtNamecomputer;
+        private Guna.UI2.WinForms.Guna2TextBox txtTypecomputer;
+    }
+}
