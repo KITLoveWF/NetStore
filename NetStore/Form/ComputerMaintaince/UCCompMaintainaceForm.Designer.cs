7188a26b410f2f4a3c0267f17e9d88774581d52b
Gia huy Nguyen <giahuy902@gmail.com>
2025-03-24 09:50:13 +0700

    edit UI

--- NetStore/Form/ComputerMaintaince/UCCompMaintainaceForm.Designer.cs
+++ NetStore/Form/ComputerMaintaince/UCCompMaintainaceForm.Designer.cs
@@ -28,30 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.ucComputerState2 = new NetStore.Form.ComputerMaintaince.UCComputerState();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.ucComputerState1 = new NetStore.Form.ComputerMaintaince.UCComputerState();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.ucDeviceReceipt1 = new NetStore.Form.ComputerMaintaince.UCDeviceReceipt();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.lblNamestaff = new System.Windows.Forms.Label();
             this.lblStatecomputer = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.ucComputerState1 = new NetStore.Form.ComputerMaintaince.UCComputerState();
+            this.ucComputerState2 = new NetStore.Form.ComputerMaintaince.UCComputerState();
+            this.ucDeviceReceipt1 = new NetStore.Form.ComputerMaintaince.UCDeviceReceipt();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.panel5.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // ucComputerState2
-            // 
-            this.ucComputerState2.Location = new System.Drawing.Point(1449, 350);
-            this.ucComputerState2.Margin = new System.Windows.Forms.Padding(9, 9, 9, 9);
-            this.ucComputerState2.Name = "ucComputerState2";
-            this.ucComputerState2.Size = new System.Drawing.Size(14, 14);
-            this.ucComputerState2.TabIndex = 9;
             // 
             // panel1
             // 
@@ -63,52 +55,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(2063, 146);
             this.panel1.TabIndex = 14;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.ucComputerState1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(0, 146);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1020, 1047);
-            this.panel2.TabIndex = 15;
-            // 
-            // ucComputerState1
-            // 
-            this.ucComputerState1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ucComputerState1.Location = new System.Drawing.Point(0, 0);
-            this.ucComputerState1.Margin = new System.Windows.Forms.Padding(9);
-            this.ucComputerState1.Name = "ucComputerState1";
-            this.ucComputerState1.Size = new System.Drawing.Size(1020, 1047);
-            this.ucComputerState1.TabIndex = 13;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.panel5);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(1020, 146);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(1043, 1047);
-            this.panel3.TabIndex = 16;
-            // 
-            // panel5
-            // 
-            this.panel5.Controls.Add(this.ucDeviceReceipt1);
-            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel5.Location = new System.Drawing.Point(0, 0);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(1043, 1047);
-            this.panel5.TabIndex = 16;
-            // 
-            // ucDeviceReceipt1
-            // 
-            this.ucDeviceReceipt1.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.ucDeviceReceipt1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.ucDeviceReceipt1.Location = new System.Drawing.Point(0, 0);
-            this.ucDeviceReceipt1.Margin = new System.Windows.Forms.Padding(9);
-            this.ucDeviceReceipt1.Name = "ucDeviceReceipt1";
-            this.ucDeviceReceipt1.Size = new System.Drawing.Size(1043, 1047);
-            this.ucDeviceReceipt1.TabIndex = 15;
             // 
             // pictureBox1
             // 
@@ -143,6 +89,60 @@
             this.lblStatecomputer.TabIndex = 6;
             this.lblStatecomputer.Text = "Tình trạng máy";
             // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.ucComputerState1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 146);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1020, 1047);
+            this.panel2.TabIndex = 15;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(1020, 146);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1043, 1047);
+            this.panel3.TabIndex = 16;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.ucDeviceReceipt1);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1043, 1047);
+            this.panel5.TabIndex = 16;
+            // 
+            // ucComputerState1
+            // 
+            this.ucComputerState1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucComputerState1.Location = new System.Drawing.Point(0, 0);
+            this.ucComputerState1.Margin = new System.Windows.Forms.Padding(9);
+            this.ucComputerState1.Name = "ucComputerState1";
+            this.ucComputerState1.Size = new System.Drawing.Size(1020, 1047);
+            this.ucComputerState1.TabIndex = 13;
+            // 
+            // ucComputerState2
+            // 
+            this.ucComputerState2.Location = new System.Drawing.Point(1449, 350);
+            this.ucComputerState2.Margin = new System.Windows.Forms.Padding(9);
+            this.ucComputerState2.Name = "ucComputerState2";
+            this.ucComputerState2.Size = new System.Drawing.Size(14, 14);
+            this.ucComputerState2.TabIndex = 9;
+            // 
+            // ucDeviceReceipt1
+            // 
+            this.ucDeviceReceipt1.BackColor = System.Drawing.Color.White;
+            this.ucDeviceReceipt1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ucDeviceReceipt1.Location = new System.Drawing.Point(0, 0);
+            this.ucDeviceReceipt1.Margin = new System.Windows.Forms.Padding(5);
+            this.ucDeviceReceipt1.Name = "ucDeviceReceipt1";
+            this.ucDeviceReceipt1.Size = new System.Drawing.Size(1043, 1047);
+            this.ucDeviceReceipt1.TabIndex = 0;
+            // 
             // UCCompMaintainaceForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
@@ -151,15 +151,15 @@
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.ucComputerState2);
-            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "UCCompMaintainaceForm";
             this.Size = new System.Drawing.Size(2063, 1193);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.panel5.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -171,9 +171,9 @@
         private UCComputerState ucComputerState1;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Panel panel5;
-        private UCDeviceReceipt ucDeviceReceipt1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lblNamestaff;
         private System.Windows.Forms.Label lblStatecomputer;
+        private UCDeviceReceipt ucDeviceReceipt1;
     }
 }
