ed03b702ded50698098eadb7a6eb0cd1716afc30
KienLoveNH <kientri098@gmail.com>
2025-04-12 15:15:04 +0700

    Login

--- NetStore/Form/FormLoginAndFoget/LoginForm.cs
+++ NetStore/Form/FormLoginAndFoget/LoginForm.cs
@@ -1,5 +1,6 @@
 ﻿using NetStore.DAO;
 using NetStore.Form.FormStateComputer;
+using NetStore.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -28,10 +29,27 @@ namespace NetStore.Form.FormLoginAndFoget
         {
             StaffDAO staffDAO = new StaffDAO();
             DataTable dt = staffDAO.FindStaff(txtUsername.Text, txtPassword.Text);
-            Console.WriteLine(dt);
-            //this.Hide();
-            //StateComputer stateComputer = new StateComputer();
-            //stateComputer.Show();
+            LoginSave.Username = txtUsername.Text;
+            LoginSave.Password = txtPassword.Text;
+            if (dt.Rows.Count > 0)
+            {
+                DataRow row = dt.Rows[0];
+
+                string info = $"ID: {row["staffID"]}\n" +
+                              $"Tên: {row["nameStaff"]}\n" +
+                              $"Username: {row["username"]}\n" +
+                              $"Password: {row["password"]}";
+
+                MessageBox.Show("Đăng nhập thành công!\n\n" + info);
+
+                this.Hide();
+                StateComputer stateComputer = new StateComputer();
+                stateComputer.Show();
+            }
+            else
+            {
+                MessageBox.Show("Sai tên đăng nhập hoặc mật khẩu!");
+            }
         }
     }
 }
