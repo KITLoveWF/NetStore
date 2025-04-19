8344ac254a7721b0eaa28c130f67c79d1a30fffe
KienLoveNH <kientri098@gmail.com>
2025-04-19 13:47:28 +0700

    Validate data customer

--- NetStore/Form/FormCustomerAdmin/AddCustomer.cs
+++ NetStore/Form/FormCustomerAdmin/AddCustomer.cs
@@ -13,7 +13,7 @@ namespace NetStore.Form.FormCustomerAdmin
 {
     public partial class AddCustomer : System.Windows.Forms.Form
     {
-        public bool isClick = false;
+        
         public AddCustomer()
         {
             InitializeComponent();
@@ -27,10 +27,22 @@ namespace NetStore.Form.FormCustomerAdmin
         private void btnSave_Click(object sender, EventArgs e)
         {
             //Customer customer =  new Customer(txtUsername.Text,txtPassword.Text);
-            var customer = new NetStore.Model.Customer(txtUsername.Text, txtPassword.Text);
-            CustomerDAO customerDAO = new CustomerDAO();
-            customerDAO.Add(customer);
-            isClick = true;
+            if(txtUsername.Text == "" ||  txtPassword.Text =="")
+            {
+                MessageBox.Show("Chưa nhập tài khoản hoặc mật khẩu!", "Tạo tài khoản",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else if(txtUsername.Text != "" && txtPassword.Text != "")
+            {
+                var customer = new NetStore.Model.Customer(txtUsername.Text, txtPassword.Text);
+                CustomerDAO customerDAO = new CustomerDAO();
+                customerDAO.Add(customer);
+                MessageBox.Show("Tạo tài khoản thành công!", "Tạo tài khoản", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.DialogResult = DialogResult.OK; // Trả về kết quả
+                this.Close();
+
+            }    
+            
+            
             //MessageBox.Show("Thêm tài khoản thành công");
         }
 
--- NetStore/Form/FormCustomerAdmin/EditCustomer.cs
+++ NetStore/Form/FormCustomerAdmin/EditCustomer.cs
@@ -28,11 +28,27 @@ namespace NetStore.Form.Customer
 
         private void btnSave_Click(object sender, EventArgs e)
         {
-            var customer = new NetStore.Model.Customer(txtUsername.Text, txtPassword.Text);
-            CustomerDAO customerDAO = new CustomerDAO();
-            customer.Id = index;
-            //MessageBox.Show($"{txtUsername.Text}");
-            customerDAO.Fix(customer);
+            if (txtUsername.Text == "" || txtPassword.Text == "")
+            {
+                MessageBox.Show("Chưa nhập tài khoản hoặc mật khẩu!", "Cập nhập tài khoản", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtUsername.Text != "" && txtPassword.Text != "")
+            {
+                DialogResult result = MessageBox.Show("Bạn có muốn cập nhật tài khoản?", "Cập nhập tài khoản", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                if(result == DialogResult.OK)
+                {
+                    var customer = new NetStore.Model.Customer(txtUsername.Text, txtPassword.Text);
+                    CustomerDAO customerDAO = new CustomerDAO();
+                    customer.Id = index;
+                    customerDAO.Fix(customer);
+                    this.DialogResult = DialogResult.OK; // Trả về kết quả
+                    this.Close();
+                }
+                
+                //MessageBox.Show($"{txtUsername.Text}");
+                
+
+            }
             isClick = true;
         }
     }
